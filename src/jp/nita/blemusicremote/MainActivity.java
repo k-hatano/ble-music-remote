ac84256666c4359fb65c47737e409205002a0387
kenta.hatano <kenta.hatano@s-cubism.jp>
2017-08-09 17:03:36 +0900

    安易に移植作業開始

--- src/jp/nita/blemusicremote/MainActivity.java
+++ src/jp/nita/blemusicremote/MainActivity.java
@@ -1,6 +1,9 @@
 package jp.nita.blemusicremote;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,11 +12,15 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
+	public static final String SERVICE_UUID = "7865087B-D9D0-423A-9C80-042D9BBEA524";
+	public static final String CHAR_UUID = "608072DD-6825-4293-B3E7-324CF0B5CA08";
+	public static final String CHAR_CONFIG_UUID = "00002902-0000-1000-8000-00805f9b34fb";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		findViewById(R.id.button_player).setOnClickListener(new View.OnClickListener() {
 
 			@Override
@@ -21,7 +28,7 @@ public class MainActivity extends Activity {
 				Intent intent = new Intent(MainActivity.this, PlayerActivity.class);
 				startActivity(intent);
 			}
-			
+
 		});
 
 		findViewById(R.id.button_controller).setOnClickListener(new View.OnClickListener() {
@@ -31,15 +38,24 @@ public class MainActivity extends Activity {
 				Intent intent = new Intent(MainActivity.this, ControllerActivity.class);
 				startActivity(intent);
 			}
-			
+
 		});
+
+		BluetoothManager bluetoothManager = (BluetoothManager) (this.getSystemService(Context.BLUETOOTH_SERVICE));
+		BluetoothAdapter adapter = bluetoothManager.getAdapter();
+
+		if ((adapter == null) || (!adapter.isEnabled())) {
+			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivity(enableBtIntent);
+			return;
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		// getMenuInflater().inflate(R.menu.main, menu);
+		return false;
 	}
 
 	@Override
